f216374dcd9a393b5b12e0679e8b78e387bf59b7
Huseyin Cicek <htcicek@users.noreply.github.com>
2019-10-01 14:11:59 -0400

    created Psl#cost, takes into account shots and coffe cps

--- lib/models/psl.rb
+++ lib/models/psl.rb
@@ -1,4 +1,12 @@
 class Psl < ActiveRecord::Base
   belongs_to :user
   belongs_to :coffee_shop
+
+  def cost
+    cost_size = coffee_shop.cost_per_size * self.size
+
+    espresso_addition = self.shots > 1 ? ((coffee_shop.cost_per_size * 3) * self.shots) : 0
+
+    cost_size + espresso_addition
+  end
 end
